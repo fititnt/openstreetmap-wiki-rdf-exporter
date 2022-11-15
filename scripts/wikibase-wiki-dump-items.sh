fe6a9ceef9388cc7fed670e06958236223ef3ef9
Emerson Rocha <rocha@ieee.org>
2022-11-15 02:30:17 -0300

    scripts/wikibase-wiki-dump-items.sh: Option to merge Ps and Qs

--- scripts/wikibase-wiki-dump-items.sh
+++ scripts/wikibase-wiki-dump-items.sh
@@ -6,8 +6,14 @@
 #         USAGE:  ./scripts/wikibase-wiki-dump-items.sh
 #                 DELAY=10 ./scripts/wikibase-wiki-dump-items.sh
 #                 Q_START=1 Q_END=2 ./scripts/wikibase-wiki-dump-items.sh
+#                 OPERATION=merge_p ./scripts/wikibase-wiki-dump-items.sh
 #
-#   DESCRIPTION:  ---
+#   DESCRIPTION:  This shell script will download Wikibase Ps and Qs in
+#                 less efficient way, one by one. Cache individual results
+#                 in disk (including errors). At the end it will merge
+#                 the output into single files, already well formated
+#                 in a preditable way (e.g. to allow diffs).
+#                 The merging mecanism may
 #
 #       OPTIONS:  env WIKI_URL_ENTITYDATA=
 #                     http://example.org/wiki/Special:EntityData/
@@ -16,8 +22,16 @@
 #                 env P_END
 #                 env Q_START
 #                 env Q_END
+#                 env CACHE_ITEMS
+#                 env CACHE_ITEMS_404
+#                 env CACHE_ITEMS
+#                 env CACHE_ITEMS_404
 #
-#  REQUIREMENTS:  - rdfpipe (pip install rdflib)
+#  REQUIREMENTS:  - curl
+#                 - rdfpipe (pip install rdflib)
+#                   - Used to merge results. Tested with rdflib 6.1.1. Feel
+#                     free to use other tools to concatenate.
+#
 #          BUGS:  ---
 #         NOTES:  ---
 #        AUTHOR:  Emerson Rocha <rocha[at]ieee.org>
@@ -32,9 +46,6 @@ set -e
 
 ROOTDIR="$(pwd)"
 
-CACHE_ITEMS="$ROOTDIR/data/cache-wiki-item-dump"
-CACHE_ITEMS_404="$ROOTDIR/data/cache-wiki-item-dump-404"
-
 #### Customizable environment variable _________________________________________
 WIKI_URL_ENTITYDATA="${WIKI_URL_ENTITYDATA:-"https://wiki.openstreetmap.org/wiki/Special:EntityData/"}"
 P_START="${P_START:-"1"}"
@@ -42,7 +53,10 @@ P_END="${P_END:-"60"}"
 Q_START="${Q_START:-"1"}"
 Q_END="${Q_END:-"20000"}"
 DELAY="${DELAY:-"5"}" # delay in seconds (after download success or error)
-RDF_INPUT_EXT="${RDF_EXT:-"ttl"}"
+CACHE_ITEMS="${CACHE_ITEMS:-"$ROOTDIR/data/cache-wiki-item-dump"}"
+CACHE_ITEMS_404="${CACHE_ITEMS_404:-"$ROOTDIR/data/cache-wiki-item-dump-404"}"
+OUTPUT_DIR="${OUTPUT_DIR:-"$ROOTDIR/data/cache"}"
+OPERATION="${OPERATION:-""}"
 
 # Usert agent used: https://meta.wikimedia.org/wiki/User-Agent_policy
 USERAGENT="${USERAGENT:-"wikibase-wiki-dump-itemsbot/0.1 (https://github.com/fititnt/openstreetmap-wiki-rdf-exporter; rocha(at)ieee.org)"}"
@@ -51,7 +65,7 @@ USERAGENT="${USERAGENT:-"wikibase-wiki-dump-itemsbot/0.1 (https://github.com/fit
 #### Fancy colors constants - - - - - - - - - - - - - - - - - - - - - - - - - -
 tty_blue=$(tput setaf 4)
 tty_green=$(tput setaf 2)
-# tty_red=$(tput setaf 1)
+tty_red=$(tput setaf 1)
 tty_normal=$(tput sgr0)
 
 ## Example
@@ -64,33 +78,47 @@ tty_normal=$(tput sgr0)
 #### functions _________________________________________________________________
 
 #######################################
-# Download existing Sophox monorepo to local cached directory.
+# Main loop. The output to screen will be a valid .tsv format. Example:
+#   item<tab>result
+#   Q1<tab>error cached
+#   Q2<tab>cached
+#   Q3<tab>downloaded
 #
 # Globals:
-#   ROOTDIR
-#   P_START
-#   P_END
-#   Q_START
-#   Q_END
+#    CACHE_ITEMS
+#    CACHE_ITEMS_404
+#
 # Arguments:
 #
 # Outputs:
 #
 #######################################
-main_loop_p() {
-  # trivium_basi="$SOPOX_LOCAL"
+main_loop_items() {
   printf "\n\t%40s\n" "${tty_blue}${FUNCNAME[0]} STARTED ${tty_normal}"
 
+  if [ ! -d "$CACHE_ITEMS" ]; then
+    printf "%s\n" "${tty_red} ERROR: env CACHE_ITEMS \
+[$CACHE_ITEMS]? ${tty_normal}"
+    exit 1
+  fi
+
+  if [ ! -d "$CACHE_ITEMS_404" ]; then
+    printf "%s\n" "${tty_red} ERROR: env CACHE_ITEMS_404 \
+[$CACHE_ITEMS_404]? ${tty_normal}"
+    exit 1
+  fi
+
+  # tab-separated output, START
   printf "\n%s\t%s" "item" "result"
 
   for ((c = P_START; c <= P_END; c++)); do
     download_wiki_item "P${c}" ""
   done
-
-  # Q1-Q500 (cached without flavor)
   for ((c = Q_START; c <= Q_END; c++)); do
     download_wiki_item "Q${c}" "?flavor=dump"
   done
+  echo ""
+  # tab-separated output, END
 
   printf "\t%40s\n" "${tty_green}${FUNCNAME[0]} FINISHED OKAY ${tty_normal}"
 }
@@ -118,9 +146,9 @@ download_wiki_item() {
 
   # https://www.wikidata.org/wiki/Wikidata:Data_access/pt-br#Less_verbose_RDF_output
 
-  if [ -f "${CACHE_ITEMS_404}/${item}.${RDF_INPUT_EXT}" ]; then
+  if [ -f "${CACHE_ITEMS_404}/${item}.ttl" ]; then
     printf "\n%s\t%s" "${item}" "error cached"
-  elif [ -f "${CACHE_ITEMS}/${item}.${RDF_INPUT_EXT}" ]; then
+  elif [ -f "${CACHE_ITEMS}/${item}.ttl" ]; then
     printf "\n%s\t%s" "${item}" "cached"
   else
     EXIT_CODE="0"
@@ -129,12 +157,12 @@ download_wiki_item() {
       --user-agent "'$USERAGENT'" \
       --silent \
       --fail \
-      --output "${CACHE_ITEMS}/${item}.${RDF_INPUT_EXT}" \
-      "${WIKI_URL_ENTITYDATA}${item}.${RDF_INPUT_EXT}${urlsuffix}" || EXIT_CODE=$?
+      --output "${CACHE_ITEMS}/${item}.ttl" \
+      "${WIKI_URL_ENTITYDATA}${item}.ttl${urlsuffix}" || EXIT_CODE=$?
     # set +x
     if [ "$EXIT_CODE" != "0" ]; then
       printf "\n%s\t%s" "${item}" "error"
-      touch "$CACHE_ITEMS_404/${item}.${RDF_INPUT_EXT}"
+      touch "$CACHE_ITEMS_404/${item}.ttl"
     else
       # printf "\n%s" "${tty_green}${item}${tty_normal}"
       printf "\n%s\t%s" "${item}" "downloaded"
@@ -146,6 +174,71 @@ download_wiki_item() {
   # printf "\t%40s\n" "${tty_green}${FUNCNAME[0]} FINISHED OKAY ${tty_normal}"
 }
 
+#######################################
+# Main loop. The output to screen will be a valid .tsv format. Example:
+#   item<tab>result
+#   Q1<tab>error cached
+#   Q2<tab>cached
+#   Q3<tab>downloaded
+#
+# Globals:
+#    CACHE_ITEMS
+#    OUTPUT_DIR
+#
+# Arguments:
+#    itemtype    string    Type of item. Values: Q , P
+# Outputs:
+#
+#######################################
+rdf_merge_items() {
+  itemtype="$1"
+  printf "\n\t%40s\n" "${tty_blue}${FUNCNAME[0]} STARTED ${tty_normal}"
+
+  if [ ! -d "$CACHE_ITEMS" ]; then
+    printf "%s\n" "${tty_red} ERROR: env CACHE_ITEMS \
+[$CACHE_ITEMS]? ${tty_normal}"
+    exit 1
+  fi
+
+  set -x
+  rdfpipe \
+    --input-format=ttl \
+    --output-format=longturtle \
+    "${CACHE_ITEMS}/${itemtype}"*.ttl \
+    >"${OUTPUT_DIR}/${itemtype}.ttl"
+  set +x
+
+  printf "\t%40s\n" "${tty_blue} INFO: [hotfixes after formating] ${tty_normal}"
+  set -x
+  # Trying to be very specific, so unlikely edit text contents
+  sed -i 's/^PREFIX schema1: /PREFIX schema: /' "${OUTPUT_DIR}/${itemtype}.ttl"
+  sed -i 's/^    a schema1:/    a schema:/g' "${OUTPUT_DIR}/${itemtype}.ttl"
+  sed -i 's/^    schema1:/    schema:/g' "${OUTPUT_DIR}/${itemtype}.ttl"
+  # Input:  PREFIX p: <file://wiki.openstreetmap.org/prop/>
+  # Output: PREFIX p: <https://wiki.openstreetmap.org/prop/>
+  sed -i 's/^PREFIX p: <file:\/\//PREFIX p: <https:\/\//g' "${OUTPUT_DIR}/${itemtype}.ttl"
+
+  # sed -r works on GNU sed (Not tested on OSX which may need sed -E instead)
+  sed -i -r 's/^PREFIX ([a-z]*): <file:\/\//PREFIX \1: <https:\/\//g' "${OUTPUT_DIR}/${itemtype}.ttl"
+  set +x
+
+  echo "TODO"
+
+  printf "\t%40s\n" "${tty_green}${FUNCNAME[0]} FINISHED OKAY ${tty_normal}"
+}
+
 #### main ______________________________________________________________________
 
-main_loop_p
+if [ -z "${OPERATION}" ] || [ "${OPERATION}" = "download" ]; then
+  main_loop_items
+fi
+
+if [ -z "${OPERATION}" ] || [ "${OPERATION}" = "merge_p" ]; then
+  # echo "TODO merge_p"
+  rdf_merge_items "P"
+fi
+
+if [ -z "${OPERATION}" ] || [ "${OPERATION}" = "merge_q" ]; then
+  # echo "TODO merge_q"
+  rdf_merge_items "Q"
+fi
